671408c1adf00190307f0681ef7dd7137446c4fe
Taras <seeyouamigo@gmail.com>
2021-02-24 12:10:41 +0200

    update1

--- src/componets/cart/Cart.jsx
+++ src/componets/cart/Cart.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import InCartItem from './InCartItem/InCartItem';
 import styles from './Cart.module.css'
 
-const Cart = ({inCart, onRemove, setInCart}) => {
-	let plusKg = (name) => {
-		console.log(name)
-	}
+const Cart = ({inCart, onRemove}) => {
+	
 	return (
 		<div className={styles.cartWrapper}>
 			<div className={styles.inCart}>
 				{inCart.length > 0
 					?   inCart.map(cart => {
-						return <InCartItem cart={cart} plusKg={plusKg} onRemove={onRemove} key={cart.id}  />
+						return <InCartItem cart={cart} onRemove={onRemove} key={cart.id}  />
 						})
 					: 'Cart is empty'
 				}
